472123d5474a38751c468a51167a1b7516136c78
PreranaPurkar <preranapurkar12@gmail.com>
2023-04-11 23:29:54 +0530

    [ADDED]:[Prerana] Address Book Project Setup with Welcome Message

---
+++ src/com/technospace/addressbook/AddressBookMain.java
@@ -0,0 +1,16 @@
+package com.technospace.addressbook;
+/***
+ * 
+ * @author Prerana Purkar
+ * AddressBookMain class performing different operations on Address Book 
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Welcome Message for user
+		System.out.println("Welcome to Address Book Program");
+	}
+
+}
---
+++ src/com/technospace/addressbook/Contacts.java
@@ -0,0 +1,15 @@
+package com.technospace.addressbook;
+
+public class Contacts {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String State;
+	private String phoneNumber;
+	private String zip;
+	private String email;
+	
+	
+
+}
